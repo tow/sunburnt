426745fc5a9f3f978ddae2fd2b89ace0cf4db2c0
Toby White <toby@scrtplns.com>
2009-07-10 01:16:34 +0100

    Do utf-8 encoding the right way round, and tidy up a bit

--- sunburnt.py
+++ sunburnt.py
@@ -11,8 +11,8 @@ import simplejson
 h = httplib2.Http(".cache")
 E = ElementMaker()
 
-def force_unicode(s):
-    if isinstance(s, unicode):
+def force_utf8(s):
+    if isinstance(s, str):
         return s
     else:
         return s.encode('utf-8')
@@ -28,8 +28,11 @@ class SolrConnection(object):
         self.update_url = self.url + "update/"
         self.select_url = self.url + "select/"
 
+    def add(self, docs):
+        self.update(self._make_update_doc(docs))
+
     def update(self, update_doc):
-        body = force_unicode(update_doc)
+        body = force_utf8(update_doc)
         headers = {"Content-Type":"text/xml; charset=utf-8"}
         r, c = h.request(self.update_url, method="POST", body=body,
                          headers=headers)
@@ -54,34 +57,7 @@ class SolrConnection(object):
                         E.field({'name':k}, v)
                         for k, v in doc.items()])
                       for doc in docs])
-        return etree.tostring(xml)
-
-    def add(self, docs):
-        self.update(self._make_update_doc(docs))
-
-
-
-solr_xml = """
-<add>
-<doc>
-  <field name="id">SOLR1000</field>
-  <field name="name">Solr, the Enterprise Search Server</field>
-  <field name="manu">Apache Software Foundation</field>
-  <field name="cat">software</field>  <field name="cat">search</field>
-  <field name="features">Advanced Full-Text Search Capabilities using Lucene</field>
-  <field name="features">Optimized for High Volume Web Traffic</field>  <field name="features">Standards Based Open Interfaces - XML and HTTP</field>
-  <field name="features">Comprehensive HTML Administration Interfaces</field>
-  <field name="features">Scalability - Efficient Replication to other Solr Search Servers</field>
-  <field name="features">Flexible and Adaptable with XML configuration and Schema</field>
-  <field name="features">Good unicode support: h&#xE9;llo (hello with an accent over the e
-)</field>
-  <field name="price">0</field>
-  <field name="popularity">10</field>
-  <field name="inStock">true</field>
-  <field name="incubationdate_dt">2006-01-17T00:00:00.000Z</field>
-</doc>
-</add>
-"""
+        return etree.tostring(xml, encoding='utf-8')
 
 
 s = SolrConnection("http://localhost:8983/solr")
