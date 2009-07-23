a1181f2b395f3e8bb7300d844d623a1d98151772
Toby White <toby@scrtplns.com>
2009-07-23 13:41:44 +0100

    Class-ify the schema parser

--- parse_schema.py
+++ parse_schema.py
@@ -3,35 +3,42 @@ import lxml.etree
 class solr_date(object):
     pass
 
-solr_data_types = {
-    'solr.StrField':str,
-    'solr.TextField':str,
-    'solr.BoolField':bool,
-    'solr.IntField':int,
-    'solr.SortableIntField':int,
-    'solr.LongField':long,
-    'solr.SortableLongField':long,
-    'solr.FloatField':float,
-    'solr.SortableFloatField':float,
-    'solr.DoubleField':float,
-    'solr.SortableDoubleField':float,
-    'solr.DateField':solr_date
-    }
+
+class SolrSchema(object):
+    solr_data_types = {
+        'solr.StrField':str,
+        'solr.TextField':str,
+        'solr.BoolField':bool,
+        'solr.IntField':int,
+        'solr.SortableIntField':int,
+        'solr.LongField':long,
+        'solr.SortableLongField':long,
+        'solr.FloatField':float,
+        'solr.SortableFloatField':float,
+        'solr.DoubleField':float,
+        'solr.SortableDoubleField':float,
+        'solr.DateField':solr_date
+        }
+
+    def __init__(self, f):
+        """initialize a schema object from a
+        filename or file-like object."""
+        self.fields = self.schema_parse(f)
+
+    def schema_parse(self, f):
+        schemadoc = lxml.etree.parse(f)
+
+        field_types = {}
+        for data_type, t in self.solr_data_types.items():
+            for field_type in schemadoc.xpath("/schema/types/fieldType[@class='%s']/@name" % data_type):
+                field_types[field_type] = t
+        schema_fields = {}
+        for field in schemadoc.xpath("/schema/fields/field"):
+            schema_fields[field.attrib['name']] = field_types.get(field.attrib['type'], 'UNKNOWN')
+
+        return schema_fields
 
 
-def schema_parse(f):
-    schemadoc = lxml.etree.parse(f)
+s = SolrSchema("/Users/tow/dl/solr/apache-solr-1.3.0/example/solr/conf/schema.xml")
 
-    field_types = {}
-    for data_type, t in solr_data_types.items():
-        for field_type in schemadoc.xpath("/schema/types/fieldType[@class='%s']/@name" % data_type):
-            field_types[field_type] = t
-
-    schema_fields = {}
-    for field in schemadoc.xpath("/schema/fields/field"):
-        schema_fields[field.attrib['name']] = field_types.get(field.attrib['type'], 'UNKNOWN')
-
-    return schema_fields
-
-
-print schema_parse("/Users/tow/dl/solr/apache-solr-1.3.0/example/solr/conf/schema.xml")
+print s.fields
