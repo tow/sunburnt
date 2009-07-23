2cc474d6151fe0002703ab849a12dfed1ecde1e4
Toby White <toby@scrtplns.com>
2009-07-23 13:37:16 +0100

    Tidy up schema manipulations

--- parse_schema.py
+++ parse_schema.py
@@ -1,32 +1,34 @@
 import lxml.etree
 
-solr_types = {str:['solr.StrField', 'solr.TextField'],
-              bool:['solr.BoolField'],
-              int:['solr.IntField', 'solr.SortableIntField'],
-              long:['solr.LongField', 'solr.SortableLongField'],
-              float:['solr.FloatField', 'solr.SortableFloatField',
-                     'solr.DoubleField', 'solr.SortableDoubleField']}
- 
+class solr_date(object):
+    pass
+
+solr_data_types = {
+    'solr.StrField':str,
+    'solr.TextField':str,
+    'solr.BoolField':bool,
+    'solr.IntField':int,
+    'solr.SortableIntField':int,
+    'solr.LongField':long,
+    'solr.SortableLongField':long,
+    'solr.FloatField':float,
+    'solr.SortableFloatField':float,
+    'solr.DoubleField':float,
+    'solr.SortableDoubleField':float,
+    'solr.DateField':solr_date
+    }
+
 schemadoc = lxml.etree.parse("/Users/tow/dl/solr/apache-solr-1.3.0/example/solr/conf/schema.xml")
 
 field_types = {}
-for t, fields in solr_types.items():
-    xpath = "/schema/types/fieldType["+" or ".join("@class='%s'"% field for field in fields)+"]/@name"
-    field_types[t] = schemadoc.xpath(xpath)
-
-inverted_field_types = {}
-for t, fields in field_types.items():
-    for field in fields:
-        inverted_field_types[field] = t
+for data_type, t in solr_data_types.items():
+    for field_type in schemadoc.xpath("/schema/types/fieldType[@class='%s']/@name" % data_type):
+        field_types[field_type] = t
 
 print field_types
-print inverted_field_types
 
 schema_fields = {}
 for field in schemadoc.xpath("/schema/fields/field"):
-    schema_fields[field.attrib['name']] = inverted_field_types.get(field.attrib['type'], 'UNKNOWN')
+    schema_fields[field.attrib['name']] = field_types.get(field.attrib['type'], 'UNKNOWN')
 
 print schema_fields
-
-#datefields = schemadoc.xpath("/schema/types/fieldType[@class='solr.DateField']/@name")
-
