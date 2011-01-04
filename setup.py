05e8bff006f5f6788e6e1b4fbfae580003e84e4b
Toby White <tow@eaddrinu.se>
2011-01-04 16:54:39 +0000

    Update setup.py & bump version number

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ import distutils.core
 
 distutils.core.setup(
     name='sunburnt',
-    version='0.3',
+    version='0.4',
     description='Python interface to Solr',
     author='Toby White',
-    author_email='toby.o.h.white@gmail.com',
+    author_email='toby@timetric.com',
     packages=['sunburnt'],
-    requires=['httplib2', 'lxml', 'pytz']
+    requires=['httplib2', 'lxml', 'pytz'],
+    license='WTFPL',
     )
