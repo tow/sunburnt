ea18339ef42ff0847e2db28c1653fa0fc23e5d5c
Toby White <tow@eaddrinu.se>
2011-05-17 13:00:27 +0100

    Explicitly export RawString from sunburnt.__init__.py

--- sunburnt/__init__.py
+++ sunburnt/__init__.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import
+
+from .strings import RawString
 from .sunburnt import SolrError, SolrInterface
 
-__all__ = ['SolrError', 'SolrInterface']
+__all__ = ['RawString', 'SolrError', 'SolrInterface']
